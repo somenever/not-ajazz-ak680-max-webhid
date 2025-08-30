4f71d31bddcfec1bfe09ab0ccc2d517bedcca5d3
someever <the.someever@gmail.com>
2025-08-30 16:46:56 +0300

    feat: remove title if running as pwa to prevent duplication

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,9 @@
 
 <svelte:head>
     <link rel="icon" href={favicon} />
-    <title>Not AJAZZ AK680 MAX Web Utility</title>
+    {#if !window.matchMedia("(display-mode: standalone)").matches}
+        <title>Not AJAZZ AK680 MAX Web Utility</title>
+    {/if}
 </svelte:head>
 
 <main class="flex h-screen w-screen items-center justify-center bg-stone-900 text-white">
