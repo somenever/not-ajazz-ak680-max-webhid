0529af55b1bf8d4852a5a471a3776a621316a67b
someever <the.someever@gmail.com>
2025-09-28 22:50:02 +0300

    feat: add toggle switch component

---
+++ src/lib/components/toggle-switch.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { clsx } from "clsx";
+
+    let { indeterminate = false, checked = $bindable(false), class: clazz = "" } = $props();
+</script>
+
+<div class={clsx("relative h-5 w-10", clazz)}>
+    <input
+        type="checkbox"
+        class={clsx(
+            "absolute inset-0 cursor-pointer appearance-none rounded-full",
+            "outline-offset-2 outline-red-600 focus-visible:outline-2",
+            indeterminate ? "bg-stone-700" : checked ? "bg-red-600" : "bg-stone-900",
+        )}
+        {indeterminate}
+        bind:checked
+    />
+    <div
+        class={clsx(
+            "pointer-events-none absolute top-1 h-3 w-3 rounded-full bg-white",
+            indeterminate ? "left-1/2 -translate-x-1/2" : checked ? "right-1" : "left-1",
+        )}
+    ></div>
+</div>
