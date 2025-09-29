47527f10042afb8c008aca9ba7a9e8c6cf660218
someever <the.someever@gmail.com>
2025-09-29 14:46:57 +0300

    feat: textbox component

---
+++ src/lib/components/textbox.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { clsx } from "clsx";
+    import type { HTMLInputAttributes } from "svelte/elements";
+
+    let {
+        value = $bindable(),
+        focused = $bindable(),
+        invalid,
+        class: clazz,
+        ...props
+    }: { invalid?: boolean; focused?: boolean; value?: string } & HTMLInputAttributes = $props();
+</script>
+
+<input
+    class={clsx(
+        "rounded-lg border-2 border-stone-500 bg-none px-2 py-1",
+        "outline-offset-2 outline-red-600 focus-visible:outline-2",
+        clazz,
+    )}
+    type="text"
+    bind:value
+    bind:focused
+    {...props}
+/>
