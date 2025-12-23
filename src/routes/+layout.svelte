1eed0de4fe4fd27b34f3f0390623188a0351ed5d
someever <the.someever@gmail.com>
2025-12-23 18:44:21 +0300

    fix(layout): set up prerendering correctly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     import "../app.css";
     import favicon from "$lib/assets/favicon.svg";
 
     let { children } = $props();
+
+    let showTitle = $state(false);
+
+    onMount(() => {
+        // Hide the title in standalone PWA
+        showTitle = !window.matchMedia("(display-mode: standalone)").matches;
+    });
 </script>
 
 <svelte:head>
     <link rel="icon" href={favicon} />
-    {#if !window.matchMedia("(display-mode: standalone)").matches}
+    {#if showTitle}
         <title>Not AJAZZ AK680 MAX Web Utility</title>
     {/if}
 </svelte:head>
