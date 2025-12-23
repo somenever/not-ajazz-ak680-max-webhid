f41640e725f8089c1e6ad9b48fa5fc1be909b24b
someever <the.someever@gmail.com>
2025-12-23 19:17:27 +0300

    feat: link favicon in app.html; improve seo and semantic tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,27 +2,16 @@
     import { onMount } from "svelte";
 
     import "../app.css";
-    import favicon from "$lib/assets/favicon.svg";
 
     let { children } = $props();
 
-    let showTitle = $state(false);
-
     onMount(() => {
-        // Hide the title in standalone PWA
-        showTitle = !window.matchMedia("(display-mode: standalone)").matches;
+        if (window.matchMedia("(display-mode: standalone)").matches) {
+            document.querySelector("title")?.remove();
+        }
     });
 </script>
 
-<svelte:head>
-    <link rel="icon" href={favicon} />
-    {#if showTitle}
-        <title>Not AJAZZ AK680 MAX Web Utility</title>
-    {/if}
-</svelte:head>
-
-<main
-    class="flex h-screen w-screen items-center justify-center overflow-y-auto bg-stone-900 text-white"
->
+<main class="flex h-screen w-screen flex-col items-center overflow-y-auto bg-stone-900 text-white">
     {@render children?.()}
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,10 @@
         ZapIcon,
     } from "@lucide/svelte";
     import { arrowsUpDownSquare } from "@lucide/lab";
+    import { onMount } from "svelte";
 
     import { nullOf } from "$lib";
+    import { storable } from "$lib/storable";
     import {
         applyKeys,
         connectKeyboard,
@@ -22,14 +24,13 @@
         setLayerPayload,
         type Keyboard,
     } from "$lib/ak680max";
-    import { storable } from "$lib/storable";
     import Button from "$lib/components/button.svelte";
     import KeyGrid from "$lib/components/key-grid.svelte";
     import Popup from "$lib/components/popup.svelte";
-    import githubLogo from "$lib/assets/github.svg";
     import ToggleButton from "$lib/components/toggle-button.svelte";
     import IconButton from "$lib/components/icon-button.svelte";
     import Textbox from "$lib/components/textbox.svelte";
+    import githubLogo from "$lib/assets/github.svg";
 
     let showDisclaimer = $state(true);
     let showUnsupportedKeyboard = $state(false);
@@ -55,7 +56,7 @@
     let keyboard = $state(nullOf<Keyboard>());
     let processingUserLock = $state(false);
 
-    $effect(() => {
+    onMount(() => {
         const callback = (event: HIDConnectionEvent) => {
             if (isAk680MaxVendorControl(event.device)) {
                 keyboard = null;
@@ -72,117 +73,119 @@
     });
 </script>
 
-<div class="flex h-full flex-col items-center">
-    <div class="flex-1"></div>
+<div class="flex-1"></div>
 
-    {#if !keyboard}
-        <div
-            class="flex max-w-140 flex-col gap-4 rounded-2xl bg-stone-800 p-8 shadow-md shadow-black/50"
+{#if !keyboard}
+    <div
+        class="flex max-w-140 flex-col gap-4 rounded-2xl bg-stone-800 p-8 shadow-md shadow-black/50"
+    >
+        <h1 class="text-2xl font-bold">Welcome to the Not AJAZZ AK680 MAX Web Utility</h1>
+        <p class="mb-2">
+            A browser-based WebHID configurator for the AJAZZ AK680 MAX keyboard. Works on Linux,
+            macOS, and Windows. Configure actuation, rapid trigger, and layers. Connect your
+            totally-not-a-keyboard by pressing the button below.
+        </p>
+        <Button
+            onclick={async () => {
+                keyboard = await connectKeyboard();
+                if (keyboard.firmwareID !== 2317) {
+                    showUnsupportedKeyboard = true;
+                }
+            }}
         >
-            <h1 class="text-2xl font-bold">Welcome to the Not AJAZZ AK680 MAX Web Utility</h1>
-            <p class="mb-6">Connect your not-a-keyboard by pressing the button below</p>
-            <Button
-                onclick={async () => {
-                    keyboard = await connectKeyboard();
-                    if (keyboard.firmwareID !== 2317) {
-                        showUnsupportedKeyboard = true;
-                    }
-                }}
-            >
-                <ZapIcon />Connect
-            </Button>
+            <ZapIcon />Connect
+        </Button>
+    </div>
+{:else}
+    <div class="flex max-w-240 flex-1 flex-col gap-4 p-4">
+        <div
+            class="flex w-full flex-col gap-2 rounded-2xl bg-stone-800 p-4 shadow-md shadow-black/50"
+        >
+            <h2 class="text-xl font-bold">AJAZZ AK680 MAX</h2>
+            <div class="flex gap-6">
+                <p class="text-green-400">Connected</p>
+                <p class="opacity-50">ID: {keyboard.firmwareID}</p>
+            </div>
         </div>
-    {:else}
-        <div class="flex max-w-240 flex-1 flex-col gap-4 p-4">
-            <div
-                class="flex w-full flex-col gap-2 rounded-2xl bg-stone-800 p-4 shadow-md shadow-black/50"
-            >
-                <h2 class="text-xl font-bold">AJAZZ AK680 MAX</h2>
-                <div class="flex gap-6">
-                    <p class="text-green-400">Connected</p>
-                    <p class="opacity-50">ID: {keyboard.firmwareID}</p>
-                </div>
-            </div>
 
-            <div class="flex flex-col gap-2 rounded-2xl bg-stone-800 p-4 shadow-md shadow-black/50">
-                <h2 class="text-lg font-semibold">Layers</h2>
-                <div class="flex flex-wrap gap-2">
-                    {#each LAYERS as layer}
-                        <Button
-                            onclick={async () => {
-                                processingUserLock = true;
-                                await send(keyboard!.device, setLayerPayload(layer));
-                                keyboard!.activeLayer = layer;
-
-                                setTimeout(async () => {
-                                    keyboard!.keys = await getKeys(keyboard!.device);
-                                    processingUserLock = false;
-                                }, 500);
-                            }}
-                            disabled={processingUserLock}
-                        >
-                            {#if keyboard!.activeLayer === layer}
-                                <CheckIcon />
-                            {/if}
-                            {$layerNames[layer] ?? `Layer ${layer + 1}`}
-                        </Button>
-                    {/each}
-                    <IconButton onclick={() => (editLayerNamesPopup = true)}>
-                        <PencilLineIcon />
-                    </IconButton>
-                </div>
-            </div>
-
-            <div class="flex flex-col gap-4 rounded-2xl bg-stone-800 p-4 shadow-md shadow-black/50">
-                <KeyGrid
-                    bind:this={keyGrid}
-                    bind:keys={keyboard.keys}
-                    onActuationChange={() => (showApplyKeysButton = true)}
-                    {showAllActuations}
-                />
-                <div class="flex gap-2">
+        <div class="flex flex-col gap-2 rounded-2xl bg-stone-800 p-4 shadow-md shadow-black/50">
+            <h2 class="text-lg font-semibold">Layers</h2>
+            <div class="flex flex-wrap gap-2">
+                {#each LAYERS as layer}
                     <Button
                         onclick={async () => {
                             processingUserLock = true;
-                            console.debug(keyboard!.keys);
-                            await applyKeys(keyboard!);
-                            processingUserLock = false;
+                            await send(keyboard!.device, setLayerPayload(layer));
+                            keyboard!.activeLayer = layer;
+
+                            setTimeout(async () => {
+                                keyboard!.keys = await getKeys(keyboard!.device);
+                                processingUserLock = false;
+                            }, 500);
                         }}
-                        disabled={processingUserLock || !showApplyKeysButton}
-                        class="flex-1"
+                        disabled={processingUserLock}
                     >
-                        <CheckIcon />Apply
+                        {#if keyboard!.activeLayer === layer}
+                            <CheckIcon />
+                        {/if}
+                        {$layerNames[layer] ?? `Layer ${layer + 1}`}
                     </Button>
-                    <IconButton onclick={() => keyGrid.selectAll()}>
-                        <SquareDashedIcon />
-                    </IconButton>
-                    <ToggleButton bind:active={showAllActuations}>
-                        <Icon iconNode={arrowsUpDownSquare} />
-                    </ToggleButton>
-                </div>
+                {/each}
+                <IconButton onclick={() => (editLayerNamesPopup = true)}>
+                    <PencilLineIcon />
+                </IconButton>
             </div>
         </div>
-    {/if}
 
-    <div class="flex-1"></div>
-
-    <div class="flex items-center justify-center p-4">
-        <span class="text-sm opacity-50">
-            made with <span class="text-red-600">❤️</span>
-            by someever
-        </span>
-        <DotIcon class="opacity-50" />
-        <span class="text-sm opacity-50">not affiliated with or endorsed by ajazz</span>
-        <DotIcon class="opacity-50" />
-        <a
-            href="https://github.com/somenever/not-ajazz-ak680-max-webhid"
-            target="_blank"
-            rel="noreferrer"
-        >
-            <img src={githubLogo} alt="GitHub logo" class="h-4 w-4" />
-        </a>
+        <div class="flex flex-col gap-4 rounded-2xl bg-stone-800 p-4 shadow-md shadow-black/50">
+            <KeyGrid
+                bind:this={keyGrid}
+                bind:keys={keyboard.keys}
+                onActuationChange={() => (showApplyKeysButton = true)}
+                {showAllActuations}
+            />
+            <div class="flex gap-2">
+                <Button
+                    onclick={async () => {
+                        processingUserLock = true;
+                        console.debug(keyboard!.keys);
+                        await applyKeys(keyboard!);
+                        processingUserLock = false;
+                    }}
+                    disabled={processingUserLock || !showApplyKeysButton}
+                    class="flex-1"
+                >
+                    <CheckIcon />Apply
+                </Button>
+                <IconButton onclick={() => keyGrid?.selectAll()}>
+                    <SquareDashedIcon />
+                </IconButton>
+                <ToggleButton bind:active={showAllActuations}>
+                    <Icon iconNode={arrowsUpDownSquare} />
+                </ToggleButton>
+            </div>
+        </div>
     </div>
-</div>
+{/if}
+
+<div class="flex-1"></div>
+
+<footer class="flex items-center justify-center p-4">
+    <span class="text-sm opacity-50">
+        made with <span class="text-red-600">❤️</span>
+        by someever
+    </span>
+    <DotIcon class="opacity-50" />
+    <span class="text-sm opacity-50">not affiliated with or endorsed by ajazz</span>
+    <DotIcon class="opacity-50" />
+    <a
+        href="https://github.com/somenever/not-ajazz-ak680-max-webhid"
+        target="_blank"
+        rel="noreferrer"
+    >
+        <img src={githubLogo} alt="GitHub logo" class="h-4 w-4" />
+    </a>
+</footer>
 
 {#if showDisclaimer}
     <Popup modal close={() => (showDisclaimer = false)} class="flex max-w-140 flex-col gap-3 p-6">
@@ -194,7 +197,8 @@
             This is experimental software. Using it will void any and all warranties from Ajazz
             Electronic Technology Co., Ltd. or any other entity. This software may permanently
             damage your keyboard. By proceeding you assume all risk and agree to hold harmless its
-            author, the copyright holder, and any and all other entities.
+            author, the copyright holder, and any and all other entities. (Realistically, it
+            probably won’t damage your keyboard, but the warning still stands.)
         </p>
         <Button onclick={() => (showDisclaimer = false)}>
             <CheckIcon />I understand
